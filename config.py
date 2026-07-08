f0b52a32f5f0edba34971ba5eeaf37c094d0a21c
KasibBot <taybawi8@gmail.com>
2026-07-08 21:16:02 +0100

    Add configuration variables for bot and database

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
+
+GROUP_ID = int(os.getenv("GROUP_ID"))
