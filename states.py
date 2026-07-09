24ce4045b6f23bb3ff07b6406135f16cb5b11309
KasibBot <taybawi8@gmail.com>
2026-07-09 15:04:38 +0100

    Update states.py

--- states.py
+++ states.py
@@ -3,3 +3,9 @@ from aiogram.fsm.state import StatesGroup, State
 
 class TaskState(StatesGroup):
     waiting_for_proof = State()
+
+
+class AdminTaskState(StatesGroup):
+    waiting_for_title = State()
+    waiting_for_url = State()
+    waiting_for_points = State()
