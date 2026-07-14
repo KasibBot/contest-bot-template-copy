86bc1b36fe80cf71c09842594216619855ec47c6
KasibBot <taybawi8@gmail.com>
2026-07-14 11:46:21 +0100

    Add new environment variables for bot configuration

--- config.py
+++ config.py
@@ -11,3 +11,9 @@ GROUP_ID = int(os.getenv("GROUP_ID"))
 
 SUPPORT_CHAT_ID = int(os.getenv("SUPPORT_CHAT_ID", "0"))
 RESULTS_GROUP_ID = int(os.getenv("RESULTS_GROUP_ID", "0"))
+
+BOT_NAME = os.getenv("BOT_NAME", "Kasib")
+
+REFERRAL_REWARD = int(os.getenv("REFERRAL_REWARD", "10"))
+
+EXCHANGE_COST = int(os.getenv("EXCHANGE_COST", "1000"))
