9ac087fab9ee61854df80870c2870dd9e819328c
KasibBot <taybawi8@gmail.com>
2026-07-14 11:23:39 +0100

    Add SUPPORT_CHAT_ID and RESULTS_GROUP_ID to config

--- config.py
+++ config.py
@@ -8,3 +8,6 @@ SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 ADMIN_ID = int(os.getenv("ADMIN_ID"))
 
 GROUP_ID = int(os.getenv("GROUP_ID"))
+
+SUPPORT_CHAT_ID = int(os.getenv("SUPPORT_CHAT_ID", "0"))
+RESULTS_GROUP_ID = int(os.getenv("RESULTS_GROUP_ID", "0"))
